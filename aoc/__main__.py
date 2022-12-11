0704c19f6c6b6d13e02c72764fddd16e5694f4fe
Vladislav Shlenskii <vladislav.shlenskii@gmail.com>
2022-12-11 17:18:59 +0400

    minor fix

--- aoc/__main__.py
+++ aoc/__main__.py
@@ -13,7 +13,9 @@ from typing_extensions import TypeAlias
 
 __parent__ = Path(__file__).resolve().parent
 
-PUZZLE_NAME_RE = re.compile(r"(?:---)?\s*Day\s+(?P<day>\d+):\s+(?P<name>.+)\s*(?:---)?")
+PUZZLE_NAME_RE = re.compile(
+    r"(?:---)?\s+?Day\s+(?P<day>\d+):\s+(?P<name>.+)\s+?(?:---)?"
+)
 
 
 Tree: TypeAlias = list["Tree"] | dict[str, "Tree"] | str
