202547ab5f8ac764a835db8343e92fda34faa625
Vladislav Shlenskii <vladislav.shlenskii@gmail.com>
2023-12-16 04:29:37 +0400

    day 6, pt 2, cpp

--- src/year_2023/day_6_wait_for_it/lang_cpp/main.cpp
+++ src/year_2023/day_6_wait_for_it/lang_cpp/main.cpp
@@ -2,32 +2,74 @@
 #define FMT_HEADER_ONLY
 
 #include "jogtrot/jogtrot.hpp"
+#include <algorithm>
 #include <charconv>
+#include <cmath>
+#include <cstdint>
 #include <fmt/core.h>
 #include <fmt/ranges.h>
+#include <optional>
 #include <ranges>
+#include <sstream>
 #include <string_view>
 #include <vector>
 
+struct QuadricEquation {
+  std::int64_t a = 0;
+  std::int64_t b = 0;
+  std::int64_t c = 0;
+
+  auto
+  solve() const noexcept -> std::optional<std::pair<double, double>> {
+    auto squared_discriminant = static_cast<std::int64_t>(b) * b - 4 * a * c;
+    if (squared_discriminant < 0) {
+      return std::nullopt;
+    }
+
+    auto discriminant = std::sqrt(squared_discriminant);
+    return std::minmax((-b - discriminant) / (2 * a), (-b + discriminant) / (2 * a));
+  }
+}; // namespace QuadricEquation
+
 struct Race {
-  int time = 0;
-  int distance = 0;
+  std::int64_t time = 0;
+  std::int64_t distance = 0;
+
+  auto
+  calc_dist_per_hold_time(int hold_time) const noexcept -> std::int64_t {
+    return hold_time * (this->time - hold_time);
+  }
+
+  auto
+  resolve_distance_trajectory() const noexcept -> QuadricEquation {
+    int f1 = this->calc_dist_per_hold_time(1);
+    int f2 = this->calc_dist_per_hold_time(2);
+
+    std::int64_t a = (f2 - 2 * f1) / 2;
+    std::int64_t b = f1 - a;
+
+    return {.a = a, .b = b, .c = 0};
+  }
 };
 
 auto
+split_row(std::string_view row) {
+  return row | std::views::split(' ') | std::views::drop(1) |
+         std::views::filter([](auto sv) { return !sv.empty(); });
+}
+
+auto
 parse_races_from_file(std::string_view filepath) -> std::vector<Race> {
-  auto splitter = [](std::string_view input) -> auto{
-    return input | std::views::split(' ') | std::views::drop(1) |
-           std::views::filter([](auto sv) { return !sv.empty(); });
-  };
   auto rows = jogtrot::read_file_rows(filepath);
 
   std::vector<Race> races{};
   for (auto [time_c, distance_c] :
-       std::views::zip(splitter(std::move(rows[0])), splitter(std::move(rows[1])))) {
+       std::views::zip(split_row(std::move(rows[0])), split_row(std::move(rows[1])))) {
 
-    int time{}, distance{};
+    int time{};
     std::from_chars(time_c.begin(), time_c.end(), time);
+
+    int distance{};
     std::from_chars(distance_c.begin(), distance_c.end(), distance);
 
     races.emplace_back(time, distance);
@@ -37,14 +79,38 @@ parse_races_from_file(std::string_view filepath) -> std::vector<Race> {
 }
 
 auto
+parse_race_from_file_with_broken_kerning(std::string_view filepath) -> Race {
+  auto rows = jogtrot::read_file_rows(filepath);
+
+  std::stringstream times_ss, distances_ss;
+  for (auto [time_c, distance_c] :
+       std::views::zip(split_row(std::move(rows[0])), split_row(std::move(rows[1])))) {
+    times_ss << std::string_view(time_c);
+    distances_ss << std::string_view(distance_c);
+  }
+
+  std::int64_t time{};
+  const auto times_s = times_ss.str();
+  std::from_chars(times_s.data(), times_s.data() + times_s.size(), time);
+
+  std::int64_t distance{};
+  const auto distances_s = distances_ss.str();
+  std::from_chars(
+      distances_s.data(), distances_s.data() + distances_s.size(), distance
+  );
+
+  return Race{.time = time, .distance = distance};
+}
+
+auto
 solve_first_part(std::string_view filepath) -> void {
   const auto races = parse_races_from_file(filepath);
 
   int solution = 1;
   for (const auto& race : races) {
     int num_wins = 0;
-    for (int hold_time : std::views::iota(1, 1 + race.time)) {
-      if (hold_time * (race.time - hold_time) > race.distance) {
+    for (std::int64_t i = 0; i < race.time; ++i) {
+      if (race.calc_dist_per_hold_time(i) > race.distance) {
         num_wins++;
       }
     }
@@ -58,7 +124,17 @@ solve_first_part(std::string_view filepath) -> void {
 }
 auto
 solve_second_part(std::string_view filepath) -> void {
-  std::string solution = fmt::format("Unimplemented. No solution for {}", filepath);
+  const auto race = parse_race_from_file_with_broken_kerning(filepath);
+  fmt::print("{} {}\n", race.time, race.distance);
+
+  auto quadric_equation = race.resolve_distance_trajectory();
+  quadric_equation.c = -race.distance;
+
+  const auto quadric_equation_solution = quadric_equation.solve();
+  auto [lhs, rhs] = quadric_equation_solution.value();
+  auto solution = 1 + static_cast<std::int64_t>(std::floor(rhs)) -
+                  static_cast<std::int64_t>(std::ceil(lhs));
+
   jogtrot::print_solution(2, solution);
 }
 
