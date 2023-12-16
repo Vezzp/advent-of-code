bf6cac44d454a021f068a07430cd622417521b6a
Vladislav Shlenskii <vladislav.shlenskii@gmail.com>
2023-12-16 18:47:01 +0400

    day 16, pt 2, go

--- src/year_2023/day_16_the_floor_will_be_lava/lang_go/main.go
+++ src/year_2023/day_16_the_floor_will_be_lava/lang_go/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"advent_of_code/jogtrot"
 )
 
@@ -31,6 +29,22 @@ func (b Beam) MoveDirection(d jogtrot.Direction) Beam {
 }
 
 type Contraption = jogtrot.Matrix[rune]
+
+func ReadContraptionFromFile(filepath string) Contraption {
+	rows := jogtrot.ReadFileRows(filepath)
+	contraption := Contraption(
+		jogtrot.NewMatrixWithShape[rune](
+			jogtrot.Shape2d{X: len(rows[0]), Y: len(rows)},
+		),
+	)
+	for y, row := range rows {
+		for x, ch := range row {
+			idx := jogtrot.RavelIndex2d(jogtrot.Coordinate2d{X: x, Y: y}, contraption.Shape)
+			contraption.Data[idx] = ch
+		}
+	}
+	return contraption
+}
 
 func EvolveBeam(beam Beam, contraption Contraption) []Beam {
 	srcIdx := jogtrot.RavelIndex2d(beam.Coordinate, contraption.Shape)
@@ -85,21 +99,7 @@ func EvolveBeam(beam Beam, contraption Contraption) []Beam {
 	return out
 }
 
-func SolveFirstPart(filepath string) {
-	rows := jogtrot.ReadFileRows(filepath)
-	contraption := Contraption(
-		jogtrot.NewMatrixWithShape[rune](
-			jogtrot.Shape2d{X: len(rows[0]), Y: len(rows)},
-		),
-	)
-	for y, row := range rows {
-		for x, ch := range row {
-			idx := jogtrot.RavelIndex2d(jogtrot.Coordinate2d{X: x, Y: y}, contraption.Shape)
-			contraption.Data[idx] = ch
-		}
-	}
-
-	startBeam := Beam{Coordinate: jogtrot.Coordinate2d{X: 0, Y: 0}, Direction: jogtrot.East}
+func EnergizeContraption(contraption Contraption, startBeam Beam) int {
 	visitedBeams := map[Beam]struct{}{startBeam: {}}
 	beams := []Beam{startBeam}
 
@@ -125,12 +125,52 @@ func SolveFirstPart(filepath string) {
 		visitedCoords[beam.Coordinate] = struct{}{}
 	}
 
-	solution := len(visitedCoords)
+	return len(visitedCoords)
+}
+
+func SolveFirstPart(filepath string) {
+	startBeam := Beam{Coordinate: jogtrot.Coordinate2d{X: 0, Y: 0}, Direction: jogtrot.East}
+	solution := EnergizeContraption(ReadContraptionFromFile(filepath), startBeam)
 	jogtrot.PrintSolution(1, solution)
 }
 
 func SolveSecondPart(filepath string) {
-	solution := fmt.Sprintf("Unimplemented. No solution for %s", filepath)
+	contraption := ReadContraptionFromFile(filepath)
+	borderCoordinates := make([]jogtrot.Coordinate2d, 0, 2*(contraption.Height()+contraption.Width()))
+	for _, y := range []int{0, contraption.Shape.Y - 1} {
+		for x := 0; x < contraption.Shape.X; x++ {
+			borderCoordinates = append(borderCoordinates, jogtrot.Coordinate2d{X: x, Y: y})
+		}
+	}
+	for _, x := range []int{0, contraption.Shape.X - 1} {
+		for y := 1; y < contraption.Shape.Y-1; y++ {
+			borderCoordinates = append(borderCoordinates, jogtrot.Coordinate2d{X: x, Y: y})
+		}
+	}
+
+	solution := 0
+	for _, coordinate := range borderCoordinates {
+		directions := []jogtrot.Direction{}
+		switch coordinate.X {
+		case 0:
+			directions = append(directions, jogtrot.East)
+		case contraption.Shape.X - 1:
+			directions = append(directions, jogtrot.West)
+		}
+
+		switch coordinate.Y {
+		case 0:
+			directions = append(directions, jogtrot.South)
+		case contraption.Shape.Y - 1:
+			directions = append(directions, jogtrot.North)
+		}
+
+		for _, direction := range directions {
+			startBeam := Beam{Coordinate: coordinate, Direction: direction}
+			solution = max(solution, EnergizeContraption(contraption, startBeam))
+		}
+	}
+
 	jogtrot.PrintSolution(2, solution)
 }
 
