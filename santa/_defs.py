9c2830116fa7780aa4cfbb231e22c6b9e72b5842
vezzp <vladislav.shlenskii@gmail.com>
2025-09-06 22:06:30 +0400

    solutions -> workshop

--- santa/_defs.py
+++ santa/_defs.py
@@ -3,6 +3,6 @@ from pathlib import Path
 
 ROOT = Path(__file__).absolute().parent.parent
 
-SOLUTIONS_ROOT = ROOT / "solutions"
+SOLUTIONS_ROOT = ROOT / "workshop"
 
 PRESENTS_ROOT = ROOT / "presents"
